1ff9ce72289fbdc6e52ef86bcb7d5d689bd33f8c
Marco Santos <marco.inctva2@gmail.com>
2021-09-06 22:12:41 -0500

    Update and bug report
    Fixed > message ""Guess a number from 1 to 15\n" changed to "Guess a number from 1 to {x}\n"
    
    (bug) > if I make the correct guess on my last life, I still get the "You lost" message.

--- Project.py
+++ Project.py
@@ -1,30 +1,39 @@
+
+#In this python project, you input a year and check whether it is a leap year or not. For this,
+#you’ll have to create a function that recognizes the pattern of leap years and can try fitting the
+#inputted year into the pattern. In the end, you can print the result using a boolean expression
+
+
+
+
 #Number_guessing > Make a program in which the computer randomly chooses a number between 1 to 10,
 #1 to 100, or any range. Then give users a hint to guess the number. Every time the user guesses
 #wrong, he gets another clue, and his score gets reduced. The clue can be multiples, divisible,
 #greater or smaller, or a combination of all
-
-
 import random
+
 
 def guess_random_number(x):
     computer_random_number = random.randint(1, x)
     guess = 0
-    lives = 6
+    lives = 6  # (bug)>if I make the correct guess on my last life, I still lose
     while guess != computer_random_number and lives > 0:
-        guess = int(input("Guess a number from 1 to 15\n"))
+        guess = int(input(f"Guess a number from 1 to {x}\n"))
         lives -= 1
         print("You have " + str(lives) + " live(s) left.")
-        if guess < computer_random_number:
+        if lives == 0:
+            print('You lost!')
+            quit()
+        elif guess < computer_random_number:
             print("That's too low, try again.")
         elif guess > computer_random_number:
             print("That's too high, try again.")
-        elif lives == 0:
-            print('You lost!')
-            quit()
+        else:
+            print("You got it!!!")
 
-guess_random_number(15)
 
-print("You got it!!!")
+guess_random_number(20)
+
 
 #madlibs
 """adj = input("adjective\n")
